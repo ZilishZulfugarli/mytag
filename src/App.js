93a28f0fa392248fc305c209cae025f6add8d5aa
ZilishZulfugarli <2002zilis@gmail.com>
2024-03-31 00:13:01 +0400

    First empty commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>Salam</div>
+      <div>Necesen</div>
+    </>
+
+  );
+}
+
+export default App;
