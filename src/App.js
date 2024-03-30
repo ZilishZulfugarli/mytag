fe8b0ab3818a9b4754c4849724262768bd665717
ZilishZulfugarli <2002zilis@gmail.com>
2024-03-31 02:05:59 +0400

    Login page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+  Link,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./pages/Login"
 
 function App() {
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="login" element={<Login/>}/>
+        </Routes>
+      </Router>
     </>
 
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import style from '../assets/styles/login/login.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock, faEye } from '@fortawesome/free-solid-svg-icons'
 
-export default function Login() {
+const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className='container'>
+        <div className='box'>
+          <div className='upperSide'>
+            <p>LOGO</p>
+            <div className='line'></div>
+            <p>Log In</p>
+          </div>
+
+          <div className='info'>
+            <span>Email</span>
+            <input type="text" placeholder='Email' />
+            <span>Password</span>
+            <input type="password" placeholder='Password'>
+              {/* <FontAwesomeIcon icon={faEye} style={{ position: "absolute" }} /> */}
+            </input>
+
+            <p className='forgot'>Forgot password?</p>
+          </div>
+
+          <div className='loginBtn'>Log in</div>
+
+          <div style={{ cursor: "pointer" }}>
+            <h4>New to POPL? <span style={{ color: "blue" }}>Create Account</span> </h4>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default Login;
+
